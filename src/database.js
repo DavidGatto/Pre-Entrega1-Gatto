db05b97a86a199cf7395a34dd9da41f538f32356
David Gatto <daviddgatto@gmail.com>
2024-03-30 22:25:45 -0300

    commander env

--- src/database.js
+++ src/database.js
@@ -1,8 +1,24 @@
 const mongoose = require("mongoose");
+const configObject = require("./config/config.js");
+const { mongo_url } = configObject;
 
-mongoose
-  .connect(
-    "mongodb+srv://daviddgatto:12345@coderdb.bnklr4n.mongodb.net/ecommerce?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("Conectado"))
-  .catch(() => console.log("Error al conectarse"));
+class BaseDatos {
+  static #instance;
+
+  constructor() {
+    mongoose.connect(mongo_url);
+  }
+
+  static getInstance() {
+    if (this.#instance) {
+      console.log("Conexion previa");
+      return this.#instance;
+    }
+
+    this.#instance = new BaseDatos();
+    console.log("Conexión exitosa");
+    return this.#instance;
+  }
+}
+
+module.exports = BaseDatos.getInstance();
